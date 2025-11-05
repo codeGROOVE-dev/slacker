e6f769a812ecd2ab75ae8b8564d8afbbd5273a25
Thomas Stromberg <t+github@codeGROOVE.dev>
2025-11-05 18:35:41 -0500

    use :large_green_circle: for outgoing

--- pkg/home/ui.go
+++ pkg/home/ui.go
@@ -140,9 +140,9 @@ func BuildPRSections(incoming, outgoing []PR) []slack.Block {
 			var indicator string
 			switch {
 			case prs[i].NeedsReview:
-				indicator = ":red_circle:"
+				indicator = ":large_red_circle:"
 			case prs[i].IsBlocked:
-				indicator = ":red_circle:"
+				indicator = ":large_red_circle:"
 			case prs[i].ActionKind != "":
 				indicator = ":speech_balloon:"
 			default:
@@ -207,9 +207,9 @@ func BuildPRSections(incoming, outgoing []PR) []slack.Block {
 			var indicator string
 			switch {
 			case prs[i].NeedsReview:
-				indicator = ":green_circle:"
+				indicator = ":large_green_circle:"
 			case prs[i].IsBlocked:
-				indicator = ":green_circle:"
+				indicator = ":large_green_circle:"
 			case prs[i].ActionKind != "":
 				indicator = ":speech_balloon:"
 			default:
